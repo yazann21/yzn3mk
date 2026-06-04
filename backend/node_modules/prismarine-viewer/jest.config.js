203b52794971caa563a60f06890ca6a4e92f1d28
yazann21 <yazanalhaddad09@gmail.com>
2026-06-04 18:19:56 +0300

    Remove node_modules and add .gitignore

--- backend/node_modules/prismarine-viewer/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'jest-puppeteer',
-  testRegex: './*\\.test\\.js$',
-  testEnvironmentOptions: require('./jest-puppeteer.config.js'),
-  testEnvironment: 'jest-environment-puppeteer'
-}
